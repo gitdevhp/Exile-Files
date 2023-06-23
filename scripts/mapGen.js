58101299c1f2d3b34d8aa14a1c96d17b9f9a6ffe
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-23 14:32:43 -0500

    test fix

--- scripts/mapGen.js
+++ scripts/mapGen.js
@@ -4,7 +4,8 @@ function initMap(){
     for (let r = 0; r < 16; r++) {
         for (let c = 0; c < array.length; c++) {
             const gridBut = grid.createElement('button');
-            gridBut.id= (r,c);
+            gridBut.setAttribute('data-x',c);
+            grid.setAttribute('data-y',r);
             gridBut.innerHTML='?';
             console.log(gridBut.id);
         }       
