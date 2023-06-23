ca2eb63d204f900748f62e9bf8f7248f4920a5d2
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-23 15:05:27 -0500

    test

--- scripts/mapGen.js
+++ scripts/mapGen.js
@@ -3,11 +3,12 @@ const grid = document.getElementById('grid');
 function initMap(){
     for (let r = 0; r < 16; r++) {
         for (let c = 0; c < array.length; c++) {
-            const gridBut = grid.createElement('button');
+            const gridBut = document.createElement('button');
             gridBut.setAttribute('data-x',c);
             grid.setAttribute('data-y',r);
             gridBut.innerHTML='?';
             console.log(gridBut.id);
+            grid.appendChild(gridBut);
         }       
     }
 }
