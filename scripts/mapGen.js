d9bf60534aea5c69a674732555b7544b9c46fe81
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-23 12:18:15 -0500

    first commit

---
+++ scripts/mapGen.js
@@ -0,0 +1,9 @@
+const grid = document.getElementById('grid');
+
+function initMap(){
+    for (let r = 0; r < 16; r++) {
+        for (let c = 0; c < array.length; c++) {
+            const gridBut = grid.createElement('button');
+        }       
+    }
+}
