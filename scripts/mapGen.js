1ebcf30e8071f3220b266a75b5463fd045858659
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-23 13:38:00 -0500

    test

--- scripts/mapGen.js
+++ scripts/mapGen.js
@@ -4,6 +4,9 @@ function initMap(){
     for (let r = 0; r < 16; r++) {
         for (let c = 0; c < array.length; c++) {
             const gridBut = grid.createElement('button');
+            gridBut.id= (r,c);
+            gridBut.innerHTML='?';
+            console.log(gridBut.id);
         }       
     }
 }
