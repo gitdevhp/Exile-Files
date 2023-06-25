c3d75844df4433c4b088794f2af3d51e1c2566b5
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-25 16:01:25 -0500

    ok

--- scripts/mapGen.js
+++ scripts/mapGen.js
@@ -4,7 +4,7 @@ const grid = document.getElementById('grid');
 var baseSet = false;
 
 let pixel_size = cnvs.width / 16;
-let num_pixels = 16*9;
+let num_pixels = 16 * 9;
 
 let curHov = null;
 
@@ -13,63 +13,92 @@ const screenHeight = window.screen.height;
 
 document.addEventListener('mousemove', e => {
     curHov = grid.elementFromPoint(e.clientX, e.clientY);
-  }, {passive: true});
+}, { passive: true });
 
 function initMap() {
     for (let r = 0; r < 16; r++) {
         for (let c = 0; c < array.length; c++) {
             const gridBut = document.createElement('div');
-            gridBut.setAttribute('data-x',c);
-            gridBut.setAttribute('data-y',r);
+            gridBut.setAttribute('data-x', c);
+            gridBut.setAttribute('data-y', r);
             gridBut.addEventListener("click", bringOption(gridBut));
             grid.appendChild(gridBut);
-        }       
+        }
     }
 }
 
-function bringOption(area){
+function bringOption(area) {
     var xArea = area.getAttribute('data-x');
     var yArea = area.getAttribute('data-y');
-    if (!baseSet){
-        for (let i = xArea-2; i < xArea+2; i++) {
+    if (!baseSet) {
+        for (let i = xArea - 2; i < xArea + 2; i++) {
             //init area Reveal
-            var vertRev = Math.abs(Math.abs((i-xArea)*2)-5);
+            var vertRev = Math.abs(Math.abs((i - xArea) * 2) - 5);
             var revX = i;
-            for (let r = yArea-((vertRev-1)/2); r < vertRev+yArea; r++) {
-                var tempHold = Math.random()*5;
-                var bonusNum = Math.pow(Math.random(),4);
+            for (let r = yArea - ((vertRev - 1) / 2); r < vertRev + yArea; r++) {
+                var tempHold = Math.random() * 5;
+                var bonusNum = Math.pow(Math.random(), 4);
                 var caseEnv;
                 switch (tempHold) {
-                    case (tempHold<1):
+                    case (tempHold < 1):
                         caseEnv = 'forest';
                         break;
-                    case (1<=tempHold<2):
+                    case (1 <= tempHold < 2):
                         caseEnv = 'mountain';
                         break;
-                    case (2<=tempHold<3):
+                    case (2 <= tempHold < 3):
                         caseEnv = 'plains';
                         break;
-                    case (3<= tempHold<4):
+                    case (3 <= tempHold < 4):
                         caseEnv = 'desert';
                         break;
                     default:
                         caseEnv = 'basic'
                 }
-                const addGs = new Gs(caseEnv, bonusNum);
+                const addGs = new Gs(caseEnv, bonusNum, true);
                 const curSelect = document.querySelector('[data-x="${i}"][data-y="${r}"]');
                 curSelect.appendChild(addGs);
+                curSelect.addEventListener('click', revealGridInfo());
             }
         }
     }
 }
 
-function Gs(env, bonus) {
+function Gs(env, bonus, reveal) {
     this.env = env;
     this.bonus = bonus;
 
-    this.reveal = false
+    this.reveal = reveal;
+    this.buildings = 'none';
 }
 
-function setBase(x, y) {
-    
+function closeGridInfo() {
+    document.getElementById('gridStat').style.display = 'none';
+}
+function revealGridInfo() {
+    console.log(curHov);
+    const gridObj = curHov[Object.keys(curHov)[0]];
+    var envu = gridObj.env;
+    document.getElementById('gName').innerHTML = 'x:' + curHov.getAttribute('data-x') + 'y:' + curHov.getAttribute('data-y');
+    document.getElementById('gEnv').innerHTML = 'Environment: ' + envu;
+    document.getElementById('gBonus').innerHTML = 'Bonus: ' + gridObj.bonus;
+    document.getElementById('gBuildings').innerHTML = 'Buildings: ' + gridObj.buildings;
+    document.getElementById('gridStat').style.display = 'block';
+    //move below to diff method
+    switch (envu) {
+        case 'forest':
+            curHov.style.backgroundColor = '#023C40';
+            break;
+        case 'mountain':
+            curHov.style.backgroundColor = '#955E42';
+            break;
+        case 'plains':
+            curHov.style.backgroundColor = '#B0970A';
+            break;
+        case 'desert':
+            curHov.style.backgroundColor = '#DAD2BC';
+            break;
+        default:
+            curHov.style.backgroundColor = '#A4958E';
+    }
 }
