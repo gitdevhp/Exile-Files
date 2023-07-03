267c3e9a4c2e5da46ffdcb6bea45006fe53790b6
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-07-02 20:39:29 -0500

    poop

--- scripts/update.js
+++ scripts/update.js
@@ -1,9 +1,14 @@
 import { rows, columns } from "/scripts/mapGen.js";
 
 function populationUpdate() {
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
+    for (let r = 1; r <= rows; r++) {
+        for (let c = 1; c <= columns; c++) {
             const curSelect = document.querySelector(`[data-x="${c}"][data-y="${r}"]`);
+            var curpop = curSelect.getAttribute('data-pop');
+            var addedPop = curpop * .5; //replace with Rate increase of population by grid
         }
     }
+    var totalPopAdd =+ addedPop;
 }
+
+setInterval(populationUpdate, 1000);
